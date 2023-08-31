e66108cfb8e969659db40e4163fb5171b4a0c71d
Lexdrane <141068597+Lexdrane@users.noreply.github.com>
2023-08-31 12:14:16 +0200

    ✨ Installation de NuxtJS + Tailwindcss

---
+++ app.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <NuxtPage />
+  </div>
+</template>
+
+<script setup lang="ts">
+useSeoMeta({
+  title: 'Noover',
+  ogTitle: 'Noover',
+  description: 'Noover est un réseau social permettant de développer ses compétences avec des tuto, events, articles, atelier tout en comuniquant avec les autres.',
+  ogDescription: 'Noover est un réseau social permettant de développer ses compétences avec des tuto, events, articles, atelier tout en comuniquant avec les autres.',
+//  ogImage: '', (pour les embed discord avec les liens, faire une petite bannière)
+  twitterCard: 'summary_large_image',
+})
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,3 @@
+<template>
+      <div class="font-bold text-xl mb-2">Soon.</div>
+</template>
